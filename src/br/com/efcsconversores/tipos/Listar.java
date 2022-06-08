42883b6643da18b0b483ac97c38f5c93a3a717f8
Edinaldo F. C. Santos <edinaldofcs@gmail.com>
2022-06-07 23:13:55 -0300

    Add JavaDoc

--- src/br/com/efcsconversores/tipos/Listar.java
+++ src/br/com/efcsconversores/tipos/Listar.java
@@ -2,6 +2,14 @@ package br.com.efcsconversores.tipos;
 
 import java.util.Collection;
 
+/**
+ * Interface que representa para gerar um contrato de listagem, recebendo uma
+ * moeda, ou uma unidade de medida
+ * 
+ * @author Edinaldo F. C .Santos
+ * @version 0.1
+ *
+ */
 public abstract interface Listar {
 		
 	public abstract <T> Collection<T> listar();
